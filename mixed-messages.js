e01e8e107f70879a0cd800d217009f6132a50565
Kat Rowe <39927664+KatRowe@users.noreply.github.com>
2021-02-09 20:58:08 +0000

    set message arrays

---
+++ mixed-messages.js
@@ -0,0 +1,8 @@
+
+// Today we will wear 'messageOne' and snack on 'messageTwo' and our activity will be 'messageThree'
+
+const messageOne = ["pjs", "fancy dress", "sports gear", "a dress"]
+
+const messageTwo = ["chocolate", "crisps", "fruit", "biscuits"]
+
+const messageThree = ["yoga", "running", "baking", "Minecraft", "watch a film", "arts and craft", "games", "puzzles"]
