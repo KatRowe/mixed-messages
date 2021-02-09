e5a4128ffb7ffed8921f36c84b11d77eae3ccadd
Kat Rowe <39927664+KatRowe@users.noreply.github.com>
2021-02-09 21:16:56 +0000

    message set with random sentence

--- mixed-messages.js
+++ mixed-messages.js
@@ -6,3 +6,13 @@ const messageOne = ["pjs", "fancy dress", "sports gear", "a dress"]
 const messageTwo = ["chocolate", "crisps", "fruit", "biscuits"]
 
 const messageThree = ["yoga", "running", "baking", "Minecraft", "watch a film", "arts and craft", "games", "puzzles"]
+
+let messageOneRandom = messageOne[Math.floor(Math.random() * messageOne.length)];
+
+let messageTwoRandom = messageTwo[Math.floor(Math.random() * messageTwo.length)];
+
+let messageThreeRandom = messageThree[Math.floor(Math.random() * messageThree.length)];
+
+
+
+console.log(`Today we will wear ${messageOneRandom} and snack on ${messageTwoRandom} and our activity will be ${messageThreeRandom}.`)
